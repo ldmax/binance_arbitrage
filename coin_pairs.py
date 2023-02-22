7231eba128132f8056b9c39bc2f2068d996221e8
ldmax <lidanmax@163.com>
2023-02-22 13:35:24 +0800

    20230221

--- coin_pairs.py
+++ coin_pairs.py
@@ -3,4 +3,13 @@ Download coin pairs via binance API
 and store in memory
 TODO: or store in JSON files?
 """
-import binance
+import yaml
+from binance import Client
+from yaml.loader import SafeLoader
+
+
+with open("testnet_keys.yaml") as f:
+    keys = yaml.load(f, Loader=SafeLoader)
+
+client = Client(keys['api_key'], keys['secret'])
+
--- oppotunity_identifier.py
+++ oppotunity_identifier.py
@@ -5,3 +5,12 @@ oppotunity (negative cycle)
 For now it's for spot market (coin-coin spot)
 If arbitrage works, try spot-future market
 """
+
+# to test dap-ui
+def add(a, b, c = 1):
+    print(a + b)
+    return a + b + c
+
+
+if __name__ == "__main__":
+    print(add(1, 2))
