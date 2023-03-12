656c17d638344952c059671bcd577d6c35439585
ldmax <lidanmax@163.com>
2023-03-12 06:33:05 +0000

    20230312

--- coin_pairs.py
+++ coin_pairs.py
@@ -6,10 +6,18 @@ TODO: or store in JSON files?
 import yaml
 from binance import Client
 from yaml.loader import SafeLoader
+from pprint import pprint
 
 
 with open("testnet_keys.yaml") as f:
     keys = yaml.load(f, Loader=SafeLoader)
 
-client = Client(keys['api_key'], keys['secret'])
+client = Client(keys['api_key'], keys['secret'], testnet=True)
 
+# get exchange info
+exchange_info = client.get_exchange_info()
+
+pprint(exchange_info)
+
+# are below pairs exausted?
+pairs = [i['symbol'] for i in exchange_info['symbols']]
